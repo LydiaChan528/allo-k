012589bbb2647ba7d87ea2d839098d7989fb6319
Matthew Tran <matthewlamtran@berkeley.edu>
2019-02-17 08:06:12 -0800

    Added more icons, finished v1.0

--- src/popup.js
+++ src/popup.js
@@ -1,25 +1,3 @@
-function save_options() {
-	var color = document.getElementById('color').value;
-	chrome.storage.sync.set({
-		favColor: color
-	}, function() {
-		var status = document.getElementById('status');
-		status.textContent = 'Options saved.';
-		setTimeout(function() { status.textContent = '' }, 750);
-	});
-}
-
-function restore_options() {
-	chrome.storage.sync.get({
-    favColor: 'red'
-	}, function(items) {
-		document.getElementById('color').value = items.favColor;
-	});
-}
-
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
-
 //idk what this does, probs just makes sure links work
 document.addEventListener('DOMContentLoaded', function () {
 	var links = document.getElementsByTagName("a");
