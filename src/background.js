b8989b98baaf2067f94a430fc64b428f728f039a
Matthew Tran <matthewlamtran@berkeley.edu>
2019-02-17 00:05:06 -0800

    Added test sound

--- src/background.js
+++ src/background.js
@@ -2,17 +2,20 @@ var options = {
 	type: "basic",
 	title: "Hello There!",
 	message: "Made with <3",
-	iconUrl: "/icons/TreeHacks-white-32.png"
+	iconUrl: "/icons/favicon_full.png",
 };
 
-alert("Hi");
-var t = setInterval(showNotif, 2000);
+//alert("Hi");
+var t = setInterval(showNotif, 2500);
 
 function showNotif() {
+	var d = new Date();
+	options.message = "h:" + d.getHours() + " m: " + d.getMinutes() + " s: " + d.getSeconds();
 	chrome.notifications.create(options, callback);
 }
 
 function callback() {
+	//alert("hai there!");
 	console.log("Notified!");
 }
 
