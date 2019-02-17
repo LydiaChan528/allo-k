2e467c248e10a445373fc1999f5361962322c2b5
Lydia Chan <lchan528@stanford.edu>
2019-02-17 07:43:01 -0800

    Implemented chrome storage on option

--- src/options.js
+++ src/options.js
@@ -4,6 +4,10 @@ function recordDefault()
   chrome.storage.sync.set({
     prof: "default"
   }, function() {});
+  function show() {
+    document.getElementById("hack_mode").style.visibility= "hidden" ;
+  }
+  show();
 }
 
 function recordDeadline()
@@ -15,7 +19,6 @@ function recordDeadline()
     document.getElementById("hack_mode").style.visibility= "visible" ;
   }
   show();
-  console.log("yeet");
 }
 
 function recordHackathon()
@@ -23,16 +26,32 @@ function recordHackathon()
   chrome.storage.sync.set({
     prof: "hackathon"
   }, function() {});
+  function show() {
+    document.getElementById("hack_mode").style.visibility= "visible" ;
+  }
+  show();
 }
+
 
 function saveData()
 {
+  console.log("yeet");
+  var hackathon = document.getElementById('hack').value;
+  var sleep = document.getElementById('sleep').value;
+  var breakfast = document.getElementById('breakfast').value;
+  var lunch = document.getElementById('lunch').value;
+  var dinner = document.getElementById('dinner').value;
+
   chrome.storage.sync.set({
-    hackathon: hackathon,
     sleep: sleep,
     breakfast: breakfast,
     lunch: lunch,
     dinner: dinner,
-    deadline: deadline,
+    deadline: hackathon,
   }, function() {});
 }
+
+document.getElementById('def1').addEventListener('click', recordDefault);
+document.getElementById('dead1').addEventListener('click', recordDeadline);
+document.getElementById('hack1').addEventListener('click', recordHackathon);
+document.getElementById('save_data').addEventListener('click', saveData);
